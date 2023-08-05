71a6a8177f9e70ebc6a733550eb96422084871c6
LucasAdriano <134859091+L7643@users.noreply.github.com>
2023-08-05 20:17:51 -0300

    fixing all

--- project/js/scripts.js
+++ project/js/scripts.js
@@ -1,11 +1,11 @@
 // script slides //
-var contador = 1;
-var img1="project/images-html/banner1.png";
-var img2="project/images-html/banner2.png";
-var img3="project/images-html/banner3.png";
-var img4="project/images-html/banner4.png";
-var tempo=2500;
-var exibir=setInterval("exibindo()",tempo);
+let contador = 1;
+let img1="project/images-html/banner1.png";
+let img2="project/images-html/banner2.png";
+let img3="project/images-html/banner3.png";
+let img4="project/images-html/banner4.png";
+let tempo=2500;
+let exibir=setInterval("exibindo()",tempo);
 
 function exibindo()
 {
@@ -26,7 +26,6 @@ function muda(x)
 }
 
 // scripts navbar //
-
 function AbreFecha(){
     if(document.getElementById("menubar").className.indexOf("invisivel") == -1)
 document.getElementById("menubar").className = 
@@ -39,10 +38,10 @@ document.getElementById("menubar").className.replace("invisivel", "visivel");
  // scripts botões valores//
 
  function aparece(){
+    let w = document.getElementById("aparece1");
+    let x = document.getElementById("aparece");
+    let y = document.getElementById("aparece2");
 
-    var w = document.getElementById("aparece1");
-    var x = document.getElementById("aparece");
-    var y = document.getElementById("aparece2");
     if (x.style.display === "none") {
         x.style.display = "block";
         w.style.display = "none";
@@ -53,10 +52,10 @@ document.getElementById("menubar").className.replace("invisivel", "visivel");
 }
 
 function aparece1(){
+    let w = document.getElementById("aparece");
+    let x = document.getElementById("aparece1");
+    let y = document.getElementById("aparece2");
 
-    var w = document.getElementById("aparece");
-    var x = document.getElementById("aparece1");
-    var y = document.getElementById("aparece2");
     if (x.style.display === "none") {
         x.style.display = "block";
         w.style.display = "none";
@@ -67,10 +66,10 @@ function aparece1(){
 }
 
 function aparece2(){
+    let w = document.getElementById("aparece");
+    let x = document.getElementById("aparece2");
+    let y = document.getElementById("aparece1");
 
-    var w = document.getElementById("aparece");
-    var x = document.getElementById("aparece2");
-    var y = document.getElementById("aparece1");
     if (x.style.display === "none") {
         x.style.display = "block";
         w.style.display = "none";
@@ -80,10 +79,11 @@ function aparece2(){
     }
 }
 
+// script do botão veja mais no cardapio
 function btncardapio(){
+    let x = document.getElementById("cardapi");
+    let w = document.getElementById("btncardapio");
 
-    var x = document.getElementById("cardapio");
-    var w = document.getElementById("btncardapio");
     if (x.style.display === "none") {
         x.style.display = "block";
         w.style.display = "none";
@@ -92,27 +92,52 @@ function btncardapio(){
     }
 }
 
+// script da validacão do cadastro//
 function valida() {
+    let rnome = document.getElementById("nome");
+    let nome = rnome.value;
+    let rtel = document.getElementById("tel");
+    let tel = rtel.value;
+    let remail = document.getElementById("email");
+    let email = remail.value;
+    let rsenha = document.getElementById("senha");
+    let senha = rsenha.value;
+    let rcep = document.getElementById("cep");
+    let cep = rcep.value;
 
-    if( !document.getElementById('nome').value, !document.getElementById('tel').value,
-     !document.getElementById('email').value, !document.getElementById('end').value, !document.getElementById('cidade').value) {  
-        alert('Preencha os campos requeridos');
+    if (nome == "") {
+        alert('Preencha o campo requerido');
+        rnome.focus();
         return false;
-    } 
+    }
+    else if (email == "") {
+        alert('Preencha o campo requerido');
+        remail.focus();
+        return false;
+    }
+    else if (senha == "" || senha.length < 8) {
+        alert('Preencha o campo requerido, com no mínimo 8 caracteres');
+        rsenha.focus();
+        return false;
+    }
+    else if (isNaN(tel) || (tel == "") || tel.length < 8) {
+        alert('Preencha o campo requerido com números');
+        rtel.focus();
+        return false;
+    }
+    else if (isNaN (cep) || (cep == "") || cep.length < 8) {
+        alert('Preencha o campo requerido com números');
+        rcep.focus();
+        return false;
+    }
     else {
         alert('Cadastro feito com sucesso!');
+        rnome.value = "";
+        remail.value = "";
+        rsenha.value = "";
+        rtel.value = "";
+        rcep.value = "";
+        location.href = "combos.html";
         return true;
-    }
-}
-
-function combos(){
-    x = document.getElementById("combos");
-    w = document.getElementById("acesso");
-
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        w.style.display = "none";
-    } else {
-        x.style.display = "none";   
     }
 }
